983a715995d9e1d7df2937fc4a5509842d7295ef
lburk87 <67616760+lburk87@users.noreply.github.com>
2020-09-15 21:32:14 -0500

    exercises, language selection

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -9,10 +9,23 @@ import org.springframework.web.bind.annotation.*;
 public class HelloController {
 
 
+//    @RequestMapping(value = "hello", method = {RequestMethod.GET, RequestMethod.POST})
+//    @ResponseBody
+//    public String hello(@RequestParam String coder) {
+//        return "Hello, " + coder + "!";
+//    }
+
     @RequestMapping(value = "hello", method = {RequestMethod.GET, RequestMethod.POST})
     @ResponseBody
-    public String hello(@RequestParam String coder) {
-        return "Hello, " + coder + "!";
+    public String hello(@RequestParam String language, @RequestParam String coder) {
+        return switch (language.toLowerCase()) {
+            case "french" -> "Bonjour, " + coder + "!";
+            case "german" -> "Guten tag, " + coder + "!";
+            case "english" -> "Hello, " + coder + "!";
+            case "spanish" -> "Hola, " + coder + "!";
+            case "kazakh" -> "Sàlemetsizbe, " + coder + "?";
+            default -> "";
+        };
     }
 
     @GetMapping
@@ -23,6 +36,13 @@ public class HelloController {
                         "<body>" +
                         "<form method = 'post' action = '/hello'>" +
                         "<input type = 'text' name = 'coder' />" +
+                        "<select name = 'language'>"+
+                        "<option value = 'English'>English</option>"+
+                        "<option value = 'French'>French</option>"+
+                        "<option value = 'Spanish'>Spanish</option>"+
+                        "<option value = 'German'>German</option>"+
+                        "<option value = 'Kazakh'>Kazakh</option>"+
+                        "</select>"+
                         "<input type = 'submit' value = 'Greet Me!' />" +
                         "</form>" +
                         "</body>" +
